e2b3e25d3be49392c8a4568a550e5b73670f2e16
Antima Singh <66549508+antimasingh070@users.noreply.github.com>
2021-10-20 20:53:17 +0530

    Create Factors Finding.cpp

---
+++ MyCode/Factors Finding.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,i,count=0;
+	cin>>n;
+	for(i=1; i<=n; i++)
+	{
+	    
+	    if(n%i==0)
+	    {
+	        count=count+1;
+	         
+	    }
+	 }
+	 cout<<count<<endl;
+	 for(i=1; i<=n; i++)
+	{
+	    
+	    if(n%i==0)
+	    {
+	        cout<<i<<" ";
+	         
+	    }
+	 }
+	
+	        
